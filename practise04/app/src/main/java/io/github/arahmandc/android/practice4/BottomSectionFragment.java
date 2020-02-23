4704871237f007e169d328bec4005f864de434be
Atikur Rahman <arahman.dcc2@gmail.com>
2020-02-23 17:03:50 +0600

    new fragment

---
+++ practise04/app/src/main/java/io/github/arahmandc/android/practice4/BottomSectionFragment.java
@@ -0,0 +1,21 @@
+package io.github.arahmandc.android.practice4;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class BottomSectionFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.bottom_section_fragment,container,false);
+        return view;
+    }
+}
